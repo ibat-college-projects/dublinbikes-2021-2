adee8dee33004b37b98829f600b16091ef31e200
John Rowley <outsidethebox@eircom.net>
2021-08-18 20:52:50 +0100

    added service code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DublinbikeComponent } from './components/dublinbike/dublinbike.component';
+import { StationDetailComponent } from './components/station-detail/station-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DublinbikeComponent,
+    StationDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
